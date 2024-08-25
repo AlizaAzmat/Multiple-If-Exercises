7afe11c979f90b0a403f10a2d4fa882cce272a61
AlizaAzmat <itxmealizay@gmail.com>
2024-08-25 17:23:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,187 @@
+
+# ==================================================Multiple If ========================
+
+# Take a character input and display values according to below given chart. - - - - 
+# If character is „S‟ then print “Sindh”. 
+# If character is „P‟ then print “Punjab”. 
+# If character is „K‟ then print “khyber pakhtunkhwa” 
+# If character is „B‟ then print “Balochistan”.
+
+# Ex 1 
+#Character
+
+# ch = input("Please enter a character")
+# if ch == 'S':
+#     print("Sindh")
+# if ch == 'P':
+#     print("Punjab")
+# if ch == 'K':
+#     print("khyber pakhtunkhwa")
+# if ch == 'B':
+#     print("Balochistan")
+
+
+
+# Take a number input and display values according to below given chart. - - - - - 
+# If number is 300 then print “Mobilink”. 
+# If string is 313 then print “Zong”. 
+# If  string is 333 then print “Ufone” 
+# If string is 345 then print “Telenor”. 
+# If string is 321 then print “Warid”. 
+
+
+
+# Ex 2
+#Number
+
+# number = int(input("Please enter a number"))
+# if number == 300:
+#   print("Mobilink")
+# if number == 313:
+#   print("Zong")
+# if number == 333:
+#   print("Ufone")
+# if number == 345:
+#   print("Telenor")
+# if number == 321:
+#   print("Warid")
+
+
+
+# Take a student name input and display values according to below given chart.  - 
+# If student name is “Ali” then print “Ali is a C# Student”. - - 
+# If student name is “Asif” then print “Asif” is a HTML Student”. 
+# If student name is “Atif” then print “Atif is a Java Student”. 
+
+
+
+
+# Ex 3
+#String
+
+
+# std_name = input("Please enter a student name")
+# if std_name == 'Ali':
+#   print("Ali is a C# Student")
+# if std_name == 'Asif':
+#   print("Asif is a HTML Student")
+# if std_name == 'Atif':
+#   print("Atif is a Java Student")
+
+
+# Take Operator name input and Display Values according to below given chart. - 
+# If Operator Name is “unary” then Print “Unary Operator deals with single 
+# operands”. - -  
+# If Operator name is “binary” then print “Binary Operator deals with two 
+# operands. 
+# If Operator name is “ternary” then print “Ternary Operator deals with three 
+# Operand”. 
+
+
+# Ex 4 
+# String 
+
+# operator_name = input("Please enter a operator name")
+# if operator_name == 'unary':
+#   print("Unary Operator deals with single operands")
+# if operator_name == 'binary':
+#   print("Binary Operator deals with two operands")
+# if operator_name == 'ternary':
+#   print("Ternary Operator deals with three operands")
+
+
+
+# Take username input and Display Values according to below given chart. - 
+# If username name is “admin” then print “WelCome Admin”. - -   
+# If username is “asif” then print “WelCome Sub User “. 
+# If username is “atif” then print “WelCome Data Entry Operator”. 
+# Take input yearofExperience and Display Values according to below given chart. - 
+# If yearofExperience is greater than or Equal 15 then Print “Most Senior”. - - - 
+
+
+#Ex 5
+# String 
+
+# user_name = input ("Please enter a user name")
+# if user_name == 'admin':
+#   print("WelCome Admin")
+# if user_name == 'asif':
+#   print("WelCome Sub User")
+# if user_name == 'atif':
+#   print("WelCome Data Entry Operator")
+
+
+# If yearofExperience is greater than or Equal 10 then Print “Senior”. 
+# If yearofExperience is greater than or Equal 5 then Print “Junior”. 
+# If yearofExperience is greater than or Equal 1 then Print “Most Junior”. 
+
+
+#Ex 6
+# Number 
+
+
+
+# yr_of_exp = int(input("Please enter a year of experience"))
+# if yr_of_exp >= 10:
+#   print("Senior")
+# elif yr_of_exp >= 5:
+#   print("Junior")
+# else:
+#   print("Most Junior")
+
+
+# Calculate Percentage and Display Grades according to below given chart. - 
+# If Percentage is greater than or Equal to 80  then Print “A+”. - - - - 
+# If percentage is greater than or Equal to 70 then Print “A”. 
+# If percentage is greater than or Equal to 60 then Print “B”. 
+# If percentage is greater than or Equal to 50 then Print “C”. 
+# If percentage is greater than or Equal to 40 then Print “D”. 
+
+
+#Ex 7
+# Number 
+
+
+# percentge_std = int(input("Please enter a percentage"))
+# if percentge_std >= 80:
+#   print("A+")
+# if percentge_std >= 70:
+#   print("A")
+# if percentge_std >= 60:
+#   print("B")
+# if percentge_std >= 50:
+#   print("C")
+# if percentge_std >= 40:
+#   print("D")
+
+
+
+
+
+# Take operator name input and display values according to below given chart. - - - 
+# If OperatorName is “Arithmetic” then Print 
+# 1. Addition 
+# 2. Subtraction 
+# 3. Multiplication 
+# 4. Division 
+# If OperatorName is “Logical” then Print 
+# 1. Logical AND ( && ) 
+# 2. Logical OR   ( | |) 
+# If OperatorName is “Relational” then Print 
+# 1. Less than  < 
+# 2. Greater than  > 
+# 3. Less than or Equal  <= 
+# 4. Greater than or Equal >= 
+
+
+
+# Ex 8 
+# String 
+
+# op_name = input("Please enter a operator name ")
+# if op_name ==  "Arithmetic":
+#   print ("1. Addition \n  2. Subtraction \n  3. Multiplication \n  4. Division ")
+# if op_name ==  "Logical":
+#   print ("1. Logical AND ( && )  \n 2. Logical OR   ( | |) ")
+# if op_name ==  "Relational":
+#   print ("1. Less than  <  \n  2. Greater than  >  \n  3. Less than or Equal  <=  \n 4. Greater than or Equal >= ")
